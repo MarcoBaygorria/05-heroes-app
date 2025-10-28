a50db9a3c489b7bdacb5361bc7c36b7b1ca26a4f
MarcoBaygorria <baygorria91@gmail.com>
2025-10-27 16:49:34 -1100

    HeroesAppRutas+Lazylout

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HeroesApp } from './HeroesApp'
+
 import './index.css'
-import { Button } from './components/ui/button'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hola mundo</h1>
-    <Button>
-      Hola de nuevo
-    </Button>
+    <HeroesApp />
   </React.StrictMode>,
 )
